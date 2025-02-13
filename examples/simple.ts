d6f8c1a343a6a03cfdeb7bcdf62ffd2f69ca52f4
Jamie DeMichele <jamie@getlibretto.com>
2025-02-13 17:25:01 -0600

    Get system messages to work (#118)

--- examples/simple.ts
+++ examples/simple.ts
@@ -6,29 +6,52 @@ async function main() {
     // apiKey: process.env.ANTHROPIC_API_KEY
   });
 
-  console.log("Testing Chat API...");
+  console.log("Testing Chat API with string system message...");
   const messages = await anthropic.messages.create({
-    messages: objectTemplate([
-      { role: "user", content: "Say this is a test to {name}" },
-    ]),
+    system: objectTemplate(
+      "You are a {tone} chatbot, greet the person by name",
+    ),
+    messages: objectTemplate([{ role: "user", content: "{name}" }]),
     max_tokens: 1024,
     model: "claude-3-haiku-20240307",
     libretto: {
-      promptTemplateName: "ts-client-test-chat",
-      templateParams: { name: "John" },
+      promptTemplateName: "ts-anthropic-test-chat-str-system",
+      templateParams: { tone: "irate", name: "John" },
     },
   });
-  console.log("Chat API replied with: ", messages.content);
+  console.log("Chat API replied with: ", messages.content[0]);
+
+  console.log("Testing Chat API with array system message...");
+  const messages2 = await anthropic.messages.create({
+    system: objectTemplate([
+      {
+        type: "text",
+        text: "You are a {tone} chatbot, greet the person by name",
+      },
+      {
+        type: "text",
+        text: "Don't forget to say you're welcome at the end too!",
+      },
+    ]),
+    messages: objectTemplate([{ role: "user", content: "{name}" }]),
+    max_tokens: 1024,
+    model: "claude-3-haiku-20240307",
+    libretto: {
+      promptTemplateName: "ts-anthropic-test-chat-arr-system",
+      templateParams: { tone: "friendly", name: "John" },
+    },
+  });
+  console.log("Chat API replied with: ", messages2.content[0]);
 
   console.log("Testing Completion API...");
   const completion = await anthropic.completions.create({
     prompt:
-      f`\n\nHuman: Say this is a test to {name}\n\nAssistant:` as unknown as string,
+      f`\n\nHuman: Say hello and greet the person by their name: {name}\n\nAssistant:` as unknown as string,
     model: "claude-2.1",
     max_tokens_to_sample: 1024,
     libretto: {
-      promptTemplateName: "ts-client-test-completion",
-      templateParams: { name: "John" },
+      promptTemplateName: "ts-anthropic-test-completion",
+      templateParams: { name: "Pacman" },
     },
   });
 
--- src/messages.ts
+++ src/messages.ts
@@ -3,9 +3,35 @@ import Core, { APIPromise } from "@anthropic-ai/sdk/core";
 import { MessageCreateParamsBase } from "@anthropic-ai/sdk/resources/messages";
 import { Stream } from "@anthropic-ai/sdk/streaming";
 import crypto from "crypto";
-import { LibrettoConfig, send_event } from ".";
+import { LibrettoConfig, objectTemplate, send_event } from ".";
 import { PiiRedactor } from "./pii";
-import { getResolvedMessages, getResolvedStream } from "./resolvers";
+import {
+  getResolvedMessages,
+  getResolvedPrompt,
+  getResolvedStream,
+} from "./resolvers";
+
+function getResolvedSystemPrompt(
+  system: _Anthropic.Messages.MessageCreateParams["system"],
+  params?: Record<string, any>,
+) {
+  if (!system) {
+    return undefined;
+  }
+
+  // if not a string, we need to create a string representation
+  let systemToUse = system;
+  if (Array.isArray(systemToUse)) {
+    // Even if the call has objectTemplate around this array, we lose that
+    // context by combining the strings, so re-wrap with objectTemplate
+    systemToUse = objectTemplate(
+      systemToUse.map((item) => item.text).join("\n"),
+    );
+  }
+
+  // Need to resolve the system prompt
+  return getResolvedPrompt(systemToUse, params);
+}
 
 export class LibrettoMessages extends _Anthropic.Messages {
   protected piiRedactor?: PiiRedactor;
@@ -53,7 +79,14 @@ export class LibrettoMessages extends _Anthropic.Messages {
     _Anthropic.Messages.Message | Stream<_Anthropic.Messages.MessageStreamEvent>
   > {
     const now = Date.now();
-    const { libretto, messages, stream, ...anthropicBody } = body;
+    const { libretto, messages, system, stream, ...anthropicBody } = body;
+
+    // Anthropic handles system seprately and outside of user/assistant messages,
+    // so we need to resolve it differently
+    const resolvedSystem = getResolvedSystemPrompt(
+      system,
+      libretto?.templateParams,
+    );
 
     const { messages: resolvedMessages, template } = getResolvedMessages(
       messages,
@@ -61,7 +94,12 @@ export class LibrettoMessages extends _Anthropic.Messages {
     );
 
     const resultPromise = super.create(
-      { ...anthropicBody, messages: resolvedMessages, stream },
+      {
+        ...anthropicBody,
+        system: resolvedSystem?.prompt,
+        messages: resolvedMessages,
+        stream,
+      },
       options,
     );
 
@@ -95,6 +133,16 @@ export class LibrettoMessages extends _Anthropic.Messages {
         }
       }
 
+      // The Sytem message needs to be prepended to template if it exists
+      const templateWithSystem: any[] = template?.map((item) => item) ?? [];
+      if (templateWithSystem?.length && resolvedSystem) {
+        const librettoSystemMsg = {
+          role: "system",
+          content: resolvedSystem.template,
+        };
+        templateWithSystem.unshift(librettoSystemMsg);
+      }
+
       await send_event({
         responseTime,
         response,
@@ -107,8 +155,7 @@ export class LibrettoMessages extends _Anthropic.Messages {
           libretto?.apiKey ??
           this.config.apiKey ??
           process.env.LIBRETTO_API_KEY,
-        promptTemplateChat:
-          libretto?.templateChat ?? template ?? resolvedMessages,
+        promptTemplateChat: templateWithSystem ?? resolvedMessages,
         promptTemplateName: resolvedPromptTemplateName,
         apiName: libretto?.promptTemplateName ?? this.config.promptTemplateName,
         prompt: {},
